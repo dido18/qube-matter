9a5e73301b885bbf0d3481dbbd9c189680e89bed
Davide <davideneri18@gmail.com>
2025-07-28 15:31:29 +0200

    number of pixels (#11)

--- sketch/qube-matter/qube.h
+++ sketch/qube-matter/qube.h
@@ -45,7 +45,7 @@ public:
     }
 
     void SetColor( uint8_t r, uint8_t g, uint8_t b, uint8_t brightness = 5){
-        for (int i = 0; i < 6; ++i) {
+        for (int i = 0; i < 8; ++i) {
             this->leds.set(i, ModulinoColor(r, g, b), brightness);
         }
         this->leds.show();
